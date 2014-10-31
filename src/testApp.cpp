c5598780d6e785daa92f0161e9c5dce3a3fdcec9
VessoVit <vesselin@vv-studio.com>
2014-10-31 13:28:53 +0100

    update docs/pages

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,20 +1,14 @@
 #include "testApp.h"
 
 
-// ###Leap Note:
-// Note on OS X, you must have this in the Run Script Build Phase of your project.
-// where the first path ../../../addons/ofxLeapMotion/ is the path to the ofxLeapMotion addon.
-
-//      cp -f ../../../addons/ofxLeapMotion/libs/lib/osx/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/libLeap.dylib"; install_name_tool -change ./libLeap.dylib @executable_path/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
-
-//   If you don't have this you'll see an error in the console: dyld: Library not loaded: @loader_path/libLeap.dylib
-
+// # Brave New Flocking
+//
 //  --------------------------------------------------------------
 void testApp::setup(){
     
     ofSetLogLevel(OF_LOG_WARNING);
     ofBackground(20);
-    
+    ofSetFrameRate(60);
     ofSetVerticalSync(true);
     
     float width = ofGetWidth() * .12;
@@ -42,9 +36,6 @@ void testApp::setup(){
 // --------- Listeners ------
 //    ofAddListener(boid.clickableEvent, this, &ofApp::onEventClicked);
 
-//ctrl.init();
-    
-    ofSetFrameRate(60);
 // we enable our gesture detection here
 	leap.open();
 	leap.setupGestures();
@@ -63,39 +54,7 @@ void testApp::update()
 //update leap hand
     fingersFound.clear();
     
-//# Leap Motion Cntrl ver.1
-//    -------------------------
-    //## simple access to the hand
-//    simpleHands = leap.getSimpleHands();
-//    
-//    if(leap.isFrameNew() && simpleHands.size() ){
-//        //Map the view in which the hand can operate
-//        //min and max of the viewpoint and mapped values on the screen
-//        leap.setMappingX(-500, 500, -ofGetWidth(), ofGetWidth());
-//        leap.setMappingY(90, 490, -ofGetHeight(), ofGetHeight());
-//        leap.setMappingZ(-150, 150, -150, 650);
-//        
-//        for(int i = 0; i < simpleHands.size(); i++){
-//            for(int j = 0; j < simpleHands[i].fingers.size(); j++){
-//                //Keep an id for all the fingers of each hand in the scene
-//                int id = simpleHands[i].fingers[j].id;
-//                //Combine the multiple into a single vector data object
-//                ofPolyline & polyline = fingerTrails[id];
-//                ofPoint pt = simpleHands[i].fingers[j].pos;
-//                
-//                //if the distance between the last point and the current is too big- lets clear the line
-//                //this stops us connecting to an old drawing
-//                if(polyline.size() && (pt-polyline[polyline.size()-1] ).length() > 50){
-//                    polyline.clear();
-//                }
-//                //add our point to our trail
-//                polyline.addVertex(pt);
-//                //store fingers seen this frame for drawing
-//                fingersFound.push_back(id);
-//            }
-//        }
-//    }
-//# Leap Motion Cntrl ver.2
+//## Leap Motion Setup
 //    -------------------------
     vector <Hand> hands = leap.getLeapHands();
     if(leap.isFrameNew() && hands.size() ){
@@ -127,83 +86,51 @@ void testApp::update()
 
 //--------------------------------------------------------------
 void testApp::draw(){
-    
-//    texture.loadImage("bnf-background.jpg");
-//    texture.getTextureReference().setTextureWrap(GL_REPEAT, GL_REPEAT);
-//    ofBackground(55);
-//    glMatrixMode(GL_TEXTURE);
-//    glPushMatrix();
-//    ofScale(2048, 1024);
-//    glMatrixMode(GL_MODELVIEW);
-//    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-    
 
     cam.begin();
     
-    ofDrawSphere(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()*2);
-
-//    texture.getTextureReference().bind();
+        ofEnableDepthTest();
+        ofEnableLighting();
+        pointLight.enable();
     
-//    cam.setDistance(10);
-//    ofRotateX(ofRadToDeg(.5));
-//    ofRotateY(ofRadToDeg(-.5));
+        ofColor(180);
+        ofNoFill();
 
-//    float spinX = sin(ofGetElapsedTimef()*.35f);
-//    float spinY = cos(ofGetElapsedTimef()*.075f);
-
-    ofEnableDepthTest();
-
-    flock.draw();
-    ofEnableLighting();
-    pointLight.enable();
-
-    ofColor(180);
-    ofNoFill();
-
+        ofDrawSphere(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()*2);
+//  Call the flocking
+        flock.draw();
 
 // Centrel object Sphere
-    ofSpherePrimitive deathStar;
-//    sphere.setPosition(ofGetWidth()*.8f, ofGetHeight()*.25, 0);
-    deathStar.setPosition(ofGetWidth()/2, ofGetHeight()/2, 0);
+        ofSpherePrimitive deathStar;
+        deathStar.setPosition(ofGetWidth()/2, ofGetHeight()/2, 0);
 //    deathStar.rotate(spinX, 1.0, 0.0, 0.0);
 //    deathStar.rotate(spinY, 0, 1.0, 0.0);
+        deathStar.setScale(1.01f);
+        deathStar.drawWireframe();
+        deathStar.setScale(1.f);
 
-// set Sphere.Wireframe
-    deathStar.setScale(1.01f);
-    deathStar.drawWireframe();
-    deathStar.setScale(1.f);
-
-    ofDisableDepthTest();
+        ofDisableDepthTest();
    
 //    for (int i = 0; i < fingersFound.size(); i++) {
 //      int id = fingersFound[i];
 //      ofPolyline & polyline = fingerTrails[id];
 //    }
-    
-    for(int i = 0; i < simpleHands.size(); i++){
-        simpleHands[i].debugDraw();
-    }
-    
+//    ### Leap Hands draw debug
+        for(int i = 0; i < simpleHands.size(); i++){
+            simpleHands[i].debugDraw();
+        }
+//    Add finger/hands trails to a single vector for drawing
 //      for(int i = 0; i < fingersFound.size(); i++){
 //          int id = fingersFound[i];
 //          ofPolyline & polyline = fingerTrails[id];
 //          // Do something with it
-//          ofDrawSphere(simpleHands.pos, 20);
-//
 //      }
 
     cam.end();
-//    ### Leap Hands draw debug
-//      for(int i = 0; i < simpleHands.size(); i++){
-//          simpleHands[i].debugDraw();
-//          simpleHands[i].handPos;
-//          ofSpherePrimitive handTracker;
-//          handTracker.setPosition(simpleHands[i].handPos);
-//    }
-
-    string distanceString;
     
-//    distanceString = "Distance: "+ofToString(cam.getDistance());
+//    ### Debug Screen Output
+    string distanceString;
+    distanceString = "Distance: "+ofToString(cam.getDistance());
     ofDrawBitmapString(distanceString, 20, 40);
     
    
